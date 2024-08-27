e446c3b4afea1bc594f30d8b5aabd59c3de9e679
Mandela Trevor <mandelatrevor@gmail.com>
2024-08-27 15:49:54 +0300

    Created base services section

---
+++ src/components/Services.jsx
@@ -0,0 +1,15 @@
+import { Section } from './Section'; 
+import { Heading } from "./Section"; 
+
+const Services = () => {
+  return (
+    <Section id='how-to-use'>
+      <div id='container'>
+        <Heading title='Generative AI made for creators'
+        text='BrainSynth unlocks the potential  of AI-powered applications'/>
+      </div>
+    </Section>
+  )
+}
+
+export default Services
