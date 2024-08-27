f6ad094213bca02819c7f83464137e3451a4543f
Mandela Trevor <mandelatrevor@gmail.com>
2024-08-27 17:56:09 +0300

    Added second element in grid tem

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -3,7 +3,12 @@ import Heading from "./design/Heading";
 import { service1, service2, service3, check } from "../assets";
 import { brainwaveServices, brainwaveServicesIcons } from "../constants";
 import Generating from "./Generating";
-import {PhotoChatMessage, Gradient, VideoChatMessage, VideoBar} from './design/Services';
+import {
+  PhotoChatMessage,
+  Gradient,
+  VideoChatMessage,
+  VideoBar,
+} from "./design/Services";
 
 const Services = () => {
   return (
@@ -59,7 +64,6 @@ const Services = () => {
             />
           </div>
 
-
           <div className="relative z-1 grid gap-5 lg:grid-cols-2 ">
             <div className="relative min-h-[39rem] border border-n-1/10 rounded-3xl overflow-hidden">
               <div className="absolute inset-0">
@@ -74,19 +78,54 @@ const Services = () => {
               <div className="absolute inset-0 flex flex-col justify-end p-9 bg-gradient-to-b from-n-8/0 to-n-8/90 lg:pt-15 ">
                 <h4 className="h4 mb-4">AI Powered Editing</h4>
                 <p className="body-2 mb-[3rem] text-n-3">
-                  Automatically enhance your photos using our AI app&apos;s music
-                  editor. <span className="text-purple-600">Try it now</span>
+                  Automatically enhance your photos using our AI app&apos;s
+                  music editor.{" "}
+                  <span className="text-purple-600">Try it now</span>
                 </p>
               </div>
               <PhotoChatMessage />
             </div>
+            <div className="p-4 bg-n-7 rounded-3xl overflow-hidden lg:min-h-[46rem]">
+              <div className="py-12 px-4 lg:px-8 ">
+                <h4 className="h4 mb-4">Video Generation</h4>
+                <p className="body-2 mb-[2rem] text-n-3 ">
+                  The world&apos;s most powerful AI Music Generation technology.
+                  What will you create?
+                </p>
+                <ul className="flex items-center justify-between">
+                  {brainwaveServicesIcons.map((item, index) => (
+                    <li
+                      className={`flex items-center justify-center rounded-2xl ${
+                        index === 2
+                          ? "w-[3rem] h-[3rem] p-0.25 bg-conic-gradient  md:w-[4.5rem] md:h-[4.5rem]"
+                          : "flex w-10 h-10 bg-n-6 md:w-15 md:h-15"
+                      } `}
+                      key={index}
+                    >
+                      <div
+                        className={
+                          index === 2
+                            ? "flex items-center justify-center w-full h-full bg-n-7 rounded-[1rem]"
+                            : ""
+                        }
+                      >
+                        <img src={item} width={24} height={24} alt={item} />
+                      </div>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+
+              <div className="relative h-[20rem] bg-n-8
+              rounded-xl overflow-hidden md:h-[25rem]">
+                <img src={service3} alt="Scary Robot Img" className="w-full h-full object-cover"
+                width={520} height={400}/>
+                <VideoChatMessage />
+                <VideoBar />
+              </div>
+            </div>
           </div>
-
-
-
-          <div>
-
-          </div>
+          <Gradient />
         </div>
       </div>
     </Section>
