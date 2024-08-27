9e0e4d79b87713cf377fec32f79b221905772206
Mandela Trevor <mandelatrevor@gmail.com>
2024-08-27 15:57:28 +0300

    Fixed import errors

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,15 +1,17 @@
-import { Section } from './Section'; 
-import { Heading } from "./Section"; 
+import Section from "./Section";
+import Heading from './design/Heading'
 
 const Services = () => {
   return (
-    <Section id='how-to-use'>
-      <div id='container'>
-        <Heading title='Generative AI made for creators'
-        text='BrainSynth unlocks the potential  of AI-powered applications'/>
+    <Section id="how-to-use">
+      <div id="container">
+        <Heading
+          title="Generative AI made for creators"
+          text="BrainSynth unlocks the potential  of AI-powered applications"
+        />
       </div>
     </Section>
-  )
-}
+  );
+};
 
-export default Services
+export default Services;
