9eb9f5cbfb7bf9cd1492e7e81fd86b8db73ec24f
Mandela Trevor <mandelatrevor@gmail.com>
2024-08-27 15:11:58 +0300

    Initial commit for site

---
+++ src/components/CompanyLogos.jsx
@@ -0,0 +1,28 @@
+/* eslint-disable react/prop-types */
+
+import { companyLogos } from "../constants";
+
+const CompanyLogos = ({ className }) => {
+  return (
+    <div className={className}>
+      <h5 className="tagline mb-6 text-center text-n-1/50">
+        Helping people create beautiful content at
+      </h5>
+      <ul className="flex">
+        {companyLogos.map((logo, idx) => (
+          <li className="flex items-center justify-center 
+          flex-1 h-[8.5rem]" key={idx}>
+            <img
+              src={logo}
+              alt={logo}
+              width={134}
+              height={25}
+            />
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default CompanyLogos;
---
+++ src/components/design/Heading.jsx
@@ -0,0 +1,10 @@
+/* eslint-disable react/prop-types */
+
+const Heading = ({ className, title }) => {
+  return <div
+    className={`${className} max-w-[50 rem] mx-auto mb-12 lg:mb-20`}>
+        {title && <h2 className="h2">{title}</h2>}
+    </div>;
+};
+
+export default Heading;
